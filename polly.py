6a5a9158bf9716fa8d67ef0ee20dc06ce5b33b89
AJurBoii <lesurea2@carleton.edu>
2023-12-01 16:00:13 -0600

    Initial Polly configuration

---
+++ polly.py
@@ -0,0 +1,14 @@
+import polly_config 
+
+'''
+In your local repo, create a polly_config.py file. Write the following and put in your own keys and region.
+
+import boto3
+
+aws_access_key_id = 'YOUR_ACCESS_KEY'
+aws_secret_access_key = 'YOUR_SECRET_KEY'
+aws_region = 'YOUR_AWS_REGION'
+
+polly = boto3.client('polly', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region)
+'''
+
