4b97047d7523041af1f69faa9cb124d4905fc875
bryce <bernsteinb@carleton.edu>
2023-12-04 12:45:09 -0600

    Test for Sky

--- api.py
+++ api.py
@@ -2,16 +2,17 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+sound_files = ['output.mp3', 'clock.mp3', 'geese.mp3', 'laser.mp3', 'orchestra.mp3']
 
 @app.route('/')
 def home():
     return '<h1>Welcome to the API</h1><p>For now, the only endpoint is <a href="/play_cards/">play_cards</a></p>'
 
 
-@app.route('/play_cards/')
-def play_cards():
+@app.route('/play_cards/<int:sound_id>/')
+def play_cards(sound_id):
     # Code for playing cards goes here
-    return send_file('output.mp3', download_name='output.mp3')  # should probably return some form of data for front end
+    return send_file(sound_files[sound_id], download_name='output.mp3')  # should probably return some form of data for front end
 
 
 if __name__ == '__main__':
